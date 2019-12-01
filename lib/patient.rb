e4d37c1f7396a474ddf1208fb4e9d62e2d77e83f
Ken Goad <goadke@g.cofc.edu>
2019-12-01 20:55:44 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def appointments                    
-    self.appointments.select do |appointment|
+    appointments.all.select do |appointment|
     appointment.patient == self
     end
   end
