46fde86a3fff9564a8d772aaec8ce9c6884b460e
Ken Goad <goadke@g.cofc.edu>
2019-12-01 20:21:11 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,22 @@
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists                    
+    self.songs.collect do |song|
+    song.artist
+  end
+  
+  end
